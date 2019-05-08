dc868ea7a6099ac8f16162a2036aef8530604184
jeyofdev <jgregoire.pro@gmail.com>
2019-05-08 15:13:27 +0200

    add libraries custom

--- gulpfile.js
+++ gulpfile.js
@@ -16,9 +16,8 @@ const imagemin = require('gulp-imagemin')
 // delete some files before each asset creation
 gulp.task('clean', () =>  {
   return del.sync([
-    'dist', 
-    'src/js/vendor.js',
-    'src/css'
+    './dist',
+    './src/css'
   ]);
 })
 
@@ -26,14 +25,35 @@ gulp.task('clean', () =>  {
 
 // optimize images
 gulp.task('img', () => {
-  return gulp.src('src/img/**/*')
+  return gulp.src('./src/img/**/*')
     .pipe(imagemin())
-    .pipe(gulp.dest('dist/img'))
+    .pipe(gulp.dest('./dist/img'))
 })
 
 
 
-// copy the js from the libraries into a single file
+// copy the files from the fonts libraries
+gulp.task('copy-fonts', () => {
+  gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/**/*')
+    .pipe(gulp.dest('./src/fonts/fontawesome/'))
+
+  gulp.src('node_modules/@fortawesome/fontawesome-free/scss/**/*')
+    .pipe(gulp.dest('./src/scss/vendor/fontawesome'))
+})
+
+
+
+// copy the css from the libraries
+gulp.task('copy-css', () => {
+  gulp.src([
+    'node_modules/bootstrap/scss/**/*'
+  ])
+  .pipe(gulp.dest('./src/scss/vendor/bootstrap'))
+})
+
+
+
+// copy the js from the libraries
 gulp.task('copy-js', () =>  {
   gulp.src([
     // add js files from used libraries
@@ -42,58 +62,48 @@ gulp.task('copy-js', () =>  {
   ])
   .pipe(concat('vendor.js'))
   .pipe(rename({suffix: '.min'}))
-  .pipe(gulp.dest('src/js/'))
-})
-
-
-
-// copy the css from the libraries into a single file
-gulp.task('copy-css', () => {
-  gulp.src([
-    // add css files from used libraries
-    'node_modules/bootstrap/dist/css/bootstrap.min.css'
-  ])
-  .pipe(concat('vendor.css'))
-  .pipe(rename({suffix: '.min'}))
-  .pipe(gulp.dest('src/css/'))
+  .pipe(gulp.dest('./src/js/'))
 })
 
 
 
 // format scss
 gulp.task('csscomb', () => {
-  return gulp.src('src/scss/app.scss')
+  return gulp.src('./src/scss/app.scss')
       .pipe(csscomb())
-      .pipe(gulp.dest('src/scss/'));
+      .pipe(gulp.dest('./src/scss/'));
 })
 
 
 
 // compile and format scss file into css
 gulp.task('css', () => {
-  gulp.src('src/scss/app.scss')
+  gulp.src('./src/scss/*.scss')
     .pipe(sass())
     .pipe(csscomb())
     .pipe(postcss())
     .on('error', sass.logError)
-    .pipe(gulp.dest('src/css'))
+    .pipe(gulp.dest('./src/css'))
 })
 
 
 
 // default task
-gulp.task('default', ['clean', 'copy-js', 'copy-css', 'csscomb', 'css', 'img'], () => {
-  gulp.src('src/*.html')
+gulp.task('default', ['clean', 'copy-fonts', 'copy-js', 'copy-css', 'csscomb', 'css', 'img'], () => {
+  gulp.src('./src/fonts/**')
+    .pipe(gulp.dest('./dist/fonts'))
+        
+  gulp.src('./src/*.html')
     .pipe(useref())
-    .pipe(gulpif('*.js', uglify()))
+    // .pipe(gulpif('*.js', uglify()))
     .pipe(gulpif('*.css', minifyCss()))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('./dist'))
 })
 
 
 
 // watch task
 gulp.task('watch', () => {
-  gulp.watch('src/scss/**/*.scss', ['csscomb']);
-  gulp.watch('src/scss/*.scss', ['css'])
+  gulp.watch('./src/scss/**/*.scss', ['csscomb']);
+  gulp.watch('./src/scss/*.scss', ['css'])
 })
