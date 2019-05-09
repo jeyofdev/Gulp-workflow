401cf702c715a20b4846fc408c1d54218e516ad4
jeyofdev <jgregoire.pro@gmail.com>
2019-05-09 15:57:25 +0200

    refactoring for gulp v4

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
-const gulp = require('gulp')
+const { src, dest, watch, series, parallel } = require('gulp')
 const del = require('del')
 const rename = require("gulp-rename")
 const concat = require('gulp-concat')
-const csscomb = require ('gulp-csscomb')
+const csscomb = require('gulp-csscomb')
 const sass = require('gulp-sass')
 const postcss = require('gulp-postcss')
 const useref = require('gulp-useref')
@@ -14,96 +14,110 @@ const imagemin = require('gulp-imagemin')
 
 
 // delete some files before each asset creation
-gulp.task('clean', () =>  {
-  return del.sync([
-    './dist',
-    './src/css'
-  ]);
-})
+async function clean () {
+  del('./dist')
+  del('./src/css')
+}
 
 
 
 // optimize images
-gulp.task('img', () => {
-  return gulp.src('./src/img/**/*')
+function images () {
+  return src('./src/img/**/*')
     .pipe(imagemin())
-    .pipe(gulp.dest('./dist/img'))
-})
+    .pipe(dest('./dist/img'))
+}
 
 
 
-// copy the files from the fonts libraries
-gulp.task('copy-fonts', () => {
-  gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/**/*')
-    .pipe(gulp.dest('./src/fonts/fontawesome/'))
+// copy the files from the fonts libraries to src folder
+async function copyFonts () {
+  src('node_modules/@fortawesome/fontawesome-free/webfonts/**/*')
+    .pipe(dest('./src/fonts/fontawesome/'))
 
-  gulp.src('node_modules/@fortawesome/fontawesome-free/scss/**/*')
-    .pipe(gulp.dest('./src/scss/vendor/fontawesome'))
-})
-
-
-
-// copy the css from the libraries
-gulp.task('copy-css', () => {
-  gulp.src([
-    'node_modules/bootstrap/scss/**/*'
-  ])
-  .pipe(gulp.dest('./src/scss/vendor/bootstrap'))
-})
+  src('node_modules/@fortawesome/fontawesome-free/scss/**/*')
+    .pipe(dest('./src/scss/vendor/fontawesome'))
+}
 
 
 
-// copy the js from the libraries
-gulp.task('copy-js', () =>  {
-  gulp.src([
+// copy the css from the libraries to src folder
+function copyCss () {
+  return src('node_modules/bootstrap/scss/**/*')
+    .pipe(dest('./src/scss/vendor/bootstrap'))
+}
+
+
+
+// copy the js from the libraries to src folder
+function copyJs () {
+  return src([
     // add js files from used libraries
     'node_modules/jquery/dist/jquery.min.js',
     'node_modules/bootstrap/dist/js/bootstrap.min.js'
   ])
   .pipe(concat('vendor.js'))
   .pipe(rename({suffix: '.min'}))
-  .pipe(gulp.dest('./src/js/'))
-})
+  .pipe(dest('./src/js/'))
+}
 
 
 
 // format scss
-gulp.task('csscomb', () => {
-  return gulp.src('./src/scss/app.scss')
+function formatScss () {
+  return src('./src/scss/app/*.scss')
       .pipe(csscomb())
-      .pipe(gulp.dest('./src/scss/'));
-})
+      .pipe(dest('./src/scss/app'));
+}
 
 
 
 // compile and format scss file into css
-gulp.task('css', () => {
-  gulp.src('./src/scss/*.scss')
+function css () {
+  return src('./src/scss/*.scss')
     .pipe(sass())
     .pipe(csscomb())
     .pipe(postcss())
     .on('error', sass.logError)
-    .pipe(gulp.dest('./src/css'))
-})
+    .pipe(dest('./src/css'))
+}
 
 
 
-// default task
-gulp.task('default', ['clean', 'copy-fonts', 'copy-js', 'copy-css', 'csscomb', 'css', 'img'], () => {
-  gulp.src('./src/fonts/**')
-    .pipe(gulp.dest('./dist/fonts'))
-        
-  gulp.src('./src/*.html')
+// copy the fonts to dist folder
+function fonts () {
+  return src('./src/fonts/**')
+    .pipe(dest('./dist/fonts'))
+}
+
+
+
+// compile html and assets files to dist folder
+function compile () {
+  return src('./src/*.html')
     .pipe(useref())
-    // .pipe(gulpif('*.js', uglify()))
+    .pipe(gulpif('*.js', uglify()))
     .pipe(gulpif('*.css', minifyCss()))
-    .pipe(gulp.dest('./dist'))
-})
+    .pipe(dest('./dist'))
+}
 
 
 
-// watch task
-gulp.task('watch', () => {
-  gulp.watch('./src/scss/**/*.scss', ['csscomb']);
-  gulp.watch('./src/scss/*.scss', ['css'])
-})
+// task watch
+function watcher () {
+  watch('./src/scss/app/*.scss', { ignoreInitial: false }, css)
+}
+
+
+
+// for run task
+module.exports = {
+  default: series(
+    clean, 
+    parallel(images, copyFonts, copyCss, copyJs), 
+    parallel(formatScss, fonts), 
+    css, 
+    compile
+  ),
+  watch: series(clean, watcher)
+}
